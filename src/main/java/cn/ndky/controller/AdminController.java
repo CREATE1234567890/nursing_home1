608653fe3da0e70bb9bcb64dd156c3757d199a3c
yang'jia'cai <2429638614@qq.com>
2023-08-01 15:13:13 +0800

    1

--- src/main/java/cn/ndky/controller/AdminController.java
+++ src/main/java/cn/ndky/controller/AdminController.java
@@ -1,7 +1,12 @@
 package cn.ndky.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.ndky.config.Result;
+import cn.ndky.mapper.AdminMapper;
+import cn.ndky.service.AdminService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
 
@@ -13,9 +18,31 @@ import org.springframework.stereotype.Controller;
  * @author yaacc
  * @since 2023-07-31
  */
-@Controller
-@RequestMapping("//admin")
-public class AdminController {
+@RestController
 
+@Slf4j
+@RequestMapping("/admin")
+public class AdminController{
+    @Autowired
+    private AdminService adminService;
+
+    //查找所有
+    @GetMapping("/all")
+    public Result<?> findAll() {
+        return Result.success(adminService.list());
+    }
+    //按id查询
+    @GetMapping("/{id}")
+
+    public Result<?> getById(@PathVariable Integer id) {
+        log.info("查询的id：{}", id);
+        return Result.success(adminService.getById(id));
+    }
+    @DeleteMapping("/{id}")
+    public Result<?> update(@PathVariable Integer id) {
+        log.info("将被删除的id：{}", id);
+        adminService.removeById(id);
+        return Result.success();
+    }
 }
 
